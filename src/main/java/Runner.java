c4686af5d454ada9ec6fd4150894134163b80913
Omer Acar <omeracar34@icloud.com>
2023-05-12 20:48:41 +0300

    v1

---
+++ src/main/java/Runner.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+
+/*
+Proje:Student Management System
+     -1-Herhangi bir eğitim kurumu için öğrenci yönetim uygulaması geliştiriniz.
+     -2-Kullanıcı
+               -öğrenci kayıt
+               -öğrenci veya öğrencileri görüntüleme
+               -id ile öğrenci güncelleme
+               -id ile öğrenci silme
+       işlemlerini yapabilmeli.
+     -3-öğrenci:id,name,lastname,city,age özelliklerine sahiptir.
+
+
+     ODEV:ogrenci silme isleminden sonra silinen ogrencinin bilgilerini goster
+     ODEV:ogrenci ekleme isleminden sonra eklenen ogrencinin bilgilerini goster
+     ODEV: -R:ad-soyadi ile ogrenci filtreleme islemlerini yapalim.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        start();
+    }
+    //1-ADIM:kullanıcıya menü gösterelim
+    //2-student classını oluşturma
+    public static void start(){
+
+        Scanner inp=new Scanner(System.in);
+        //10-service ve tablo oluşturma
+        StudentService service=new StudentService();
+        service.createStudentTable();
+
+        int select;
+        int id;
+        do {
+            System.out.println("=====================================");
+            System.out.println("Öğrenci Yönetim Paneli");
+            System.out.println("1-Öğrenci Kayıt");
+            System.out.println("2-Tüm Öğrencileri Listele");
+            System.out.println("3-Öğrenci Güncelle");
+            System.out.println("4-Öğrenci Sil");
+            System.out.println("5-Ogreniyi gosterme");
+            System.out.println("0-ÇIKIŞ");
+            System.out.println("İşlem Seçiniz: ");
+            select=inp.nextInt();
+            inp.nextLine();
+            switch (select){
+                case 1:
+                    //11-Öğrenci Kayıt
+                    service.saveStudent();
+                    service.getAllStudent();
+                    break;
+                case 2:
+                    service.getAllStudent();
+                    //13-Tüm Öğrencileri Listele
+                    break;
+                case 3:
+                    //16-tablodan idsi verilen kaydi silme
+                    //  System.out.println("Öğrenci Id: ");
+                    id=getId(inp);
+                    //Öğrenci Güncelle
+                    service.updateStudent(id);
+
+                    break;
+                case 4:
+                    id=getId(inp);
+                    //15-öğrenci silme
+                    service.deleteStudent(id);
+                    service.getAllStudent();
+                    break;
+                case 5:
+                id=getId(inp);
+                //21-ogrenciyi bulma
+                service.displayStudent(id);
+                    break;
+                case 0:
+                    System.out.println("İyi günler...");
+                    break;
+                default:
+                    System.out.println("Hatalı giriş!!!");
+                    break;
+            }
+
+        }while(select!=0);
+
+    }
+
+    private static int getId(Scanner inp){
+        System.out.println("Öğrenci Id: ");
+        int id=inp.nextInt();
+        inp.nextLine();
+        return id;
+    }
+
+}
+
---
+++ src/main/java/StudentService.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+
+//3-student ile ilgili metodlar
+//4-DB e bağlanma, DB işlemleri için yeni bir repo class oluşturma
+public class StudentService {
+
+    Scanner inp=new Scanner(System.in);
+
+    //repository classındaki metodları kullanabilmek için obje oluşturalım.
+    private StudentRepository repo=new StudentRepository();
+
+    //9-student için tablo oluşturma
+    public void createStudentTable(){
+        repo.createTable();
+    }
+
+    //11-öğrenciyi kaydetme
+    public void saveStudent(){
+        System.out.println("AD: ");//length(" asd   ")=7 trim("     ")->""
+        String name=inp.nextLine().trim();//asd
+        System.out.println("SOYAD: ");
+        String lastname=inp.nextLine().trim();
+        System.out.println("ŞEHİR: ");
+        String city=inp.nextLine().trim();
+        System.out.println("YAŞ: ");
+        int age= inp.nextInt();
+        inp.nextLine();
+        Student newStudent=new Student(name,lastname,city,age);
+        repo.save(newStudent);
+    }
+
+    //13-Tum ogrencileri listeleme
+    public void getAllStudent() {
+        repo.findAll();
+    }
+
+
+    //15-id si verilen studenti silme
+    public void deleteStudent(int id) {
+        repo.delete(id);
+    }
+
+    //17-id si verilen ogrenciyi bulma
+    public Student getStudentById(int id){
+        Student student=repo.findById(id);
+        return student;
+    }
+
+    //21-id si verilen ogrenciyi goruntuleme
+    public void displayStudent(int id) {
+        Student student=getStudentById(id);
+        if (student==null){
+            System.out.println("Student does not exist by id:"+id);
+        }else {
+            System.out.println(student);
+        }
+    }
+
+    //19-id si verilen ogrenciyi guncelleme
+    public void updateStudent(int id){
+        //bu id ile kayitli id mevcut mu
+        Student foundStudent=getStudentById(id);
+        if (foundStudent==null){
+            System.out.println("Student does not exist by id:"+id);
+        }else{
+            System.out.println("AD: ");
+            String name=inp.nextLine().trim();//asd
+            System.out.println("SOYAD: ");
+            String lastname=inp.nextLine().trim();
+            System.out.println("ŞEHİR: ");
+            String city=inp.nextLine().trim();
+            System.out.println("YAŞ: ");
+            int age= inp.nextInt();
+            inp.nextLine();
+            //bulunan ogrencinin ozelliklerini guncelle
+            foundStudent.setName(name);
+            foundStudent.setLastname(lastname);
+            foundStudent.setCity(city);
+            foundStudent.setAge(age);
+            //id si aynı kalacak
+
+            repo.update(foundStudent);
+        }
+
+
+    }
+
+
+}
